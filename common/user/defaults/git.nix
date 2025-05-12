a239eabd2c30f6cd0e5f37f5b780edcb450a0ee9
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-05-12 00:10:22 +0000

    Added Git

--- common/user/defaults/git.nix
+++ common/user/defaults/git.nix
@@ -7,7 +7,7 @@
 
     # Optional: Set default branch name for new repositories
     extraConfig = {
-      "init.defaultBranch" = "main";
+      init.defaultBranch = "main";
       # You can add other global .gitconfig settings here
       # "core.editor" = config.my.preferredEditor; # Let EDITOR env var handle this or set explicitly
       # "core.pager" = "delta"; # If you install and prefer git-delta
